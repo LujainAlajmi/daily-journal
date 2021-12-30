9ec405c861f9ea51fc0d5cfa262c03b0638c813a
Lujain <82683327+LujainAlajmi@users.noreply.github.com>
2021-12-30 17:21:12 +0300

    moving to mongo atles, adding authorization to display posts to specific users, css is not complete

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+
 const port = process.env.PORT || 3000;
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,15 +24,140 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
+mongoose.connect(
+ process.env.DB_LINK
+);
 
-let posts=[];
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-app.get("/", function(req, res) {
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
+app.use(passport.initialize());
+app.use(passport.session());
+
+const postsSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  username:String
+});
+const userSchema = new mongoose.Schema({
+  // email: String,
+  password: String,
+  googleId: String,
+  username: String,
+  post: String,
+});
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+const Post = new mongoose.model("Post", postsSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/home",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id, username: profile._json.email}, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile","email"] })
+);
+
+app.get(
+  "/auth/google/home",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/home");
+  }
+);
+
+app.get("/", function (req, res) {
+  res.redirect("/home");
+});
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+
+
+app.post("/register", function (req, res) {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/home");
+        });
+      }
+    }
+  );
+});
+
+app.post("/login", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
   });
 
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/home");
+      });
+    }
+  });
+});
+
+
+
+app.get("/home", function (req, res) {
+  if (req.isAuthenticated()) {
+    Post.find({username: req.user.username}, function (err, foundPosts) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", { startingContent: homeStartingContent, posts: foundPosts });
+    }
+  });
+  } else {
+    res.redirect("/login");
+  }  
 });
 
 app.get("/about", function(req, res) {
@@ -42,39 +174,59 @@ app.get("/contact", function(req, res) {
 });
 
 app.get("/compose", function(req, res) {
-  res.render("compose");
-});
-
-app.post("/compose", function(req, res) {
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
-posts.push(post);
-res.redirect("/");
-});
-
-app.get("/posts/:postName",function(req,res){
-
-const requestedTitle = _.lowerCase(req.params.postName);
-
-posts.forEach(function(post){
-  const storedTitle = _.lowerCase(post.title);
-  if(storedTitle===requestedTitle){
-    res.render("post", {
-      title: post.title,
-      content: post.content
-    });
+  if (req.isAuthenticated()) {
+    res.render("compose");
+  } else {
+    res.redirect("/login");
   }
 });
 
+app.post("/compose", function (req, res) {
+
+  const submittedTitle = req.body.postTitle;
+  
+User.findById(req.user.id, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.post = submittedTitle;
+        const post = new Post({
+        title: req.body.postTitle,
+          content: req.body.postBody,
+          username: req.user.username
+        });
+        post.save();
+        foundUser.save(function () {
+        res.redirect("/home");
+        });
+      }
+    }
+  });
 });
 
 
 
+app.get("/posts/:postName",function(req,res){
+
+  const requestedTitle = _.lowerCase(req.params.postName);
+  Post.findOne({ title: requestedTitle }, function (err, foundPost) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("post", {
+      title: foundPost.title,
+      content: foundPost.content
+    });
+    }
+  });
+});
 
 
-
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/login");
+});
 
 app.listen(port, function() {
   console.log("Server started on port 3000");
